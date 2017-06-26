0cdbfdded961b869cd908844ae73a73d2c98c7c4
Eric Kidder <eric.kidder@pgi.com>
2017-06-25 21:01:55 -0400

    Split spec files into client/server

---
+++ spec/client.spec.ts
@@ -0,0 +1,29 @@
+/* tslint:disable:no-unused-expression */
+
+import Telnet from '../src/telnet';
+
+import sinon = require('sinon');
+import chai = require('chai');
+import sinonChai = require('sinon-chai');
+
+const expect = chai.expect;
+
+before(() => {
+    chai.use(sinonChai);
+});
+
+beforeEach(() => {
+    this.sandbox = sinon.sandbox.create();
+});
+
+afterEach(() => {
+    this.sandbox.restore();
+});
+
+describe('Telnet.client', () => {
+    const TEST_URL = 'telnet://localhost:9999';
+    it('should create a client object', () => {
+        const telnet = Telnet.client(TEST_URL, {});
+        expect(telnet).to.not.be.null;
+    });
+});
