0611dfd3e2d5161e40a731ac2934bdce9e7aa7c4
herrevilkitten <herrevilkitten@users.noreply.github.com>
2018-01-23 22:24:03 -0500

    Changed the code to work with newer typescript.

--- src/connection.ts
+++ src/connection.ts
@@ -41,7 +41,7 @@ export class Connection extends ReplaySubject<Event> {
      */
     get data(): Observable<string> {
         return this.filter((event: Event) => event instanceof Event.Data)
-            .map((event: Event.Data) => event.data);
+            .map((event) => (event as Event.Data).data);
     }
 
     /**
@@ -49,7 +49,7 @@ export class Connection extends ReplaySubject<Event> {
      */
     get commands(): Observable<number[]> {
         return this.filter((event: Event) => event instanceof Event.Command)
-            .map((event: Event.Command) => event.command);
+            .map((event) => (event as Event.Command).command);
     }
 
     /**
